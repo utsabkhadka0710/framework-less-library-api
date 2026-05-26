520f2d67aa536c17358962096e0707d174036971
Utsab <utsabkhadka9475@gmail.com>
2026-05-26 15:30:25 +0545

    update: removed rocket emoji on /home endpoint response

--- app/handlers/home.py
+++ app/handlers/home.py
@@ -2,5 +2,5 @@ def home_handler(query=None,params=None):
     return 200, {
         "status": "success",
         "mesage": "A RESTful CRUD API for managing a library system — built entirely in **pure Python**, with no external backend frameworks like Django or FastAPI. Uses Python's built-in `http.server` module and connects to a **PostgreSQL** database hosted on NeonDB.",
-        "live API on 🚀": "🚀 https://framework-less-library-api-utsab.onrender.com"
+        "live API on": "https://framework-less-library-api-utsab.onrender.com"
         }
