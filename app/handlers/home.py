9fb236a55a80855dcdbf26fc6b8501436834cb31
Utsab <utsabkhadka9475@gmail.com>
2026-05-26 15:14:20 +0545

    update: update the response for /home endpoint

--- app/handlers/home.py
+++ app/handlers/home.py
@@ -1,3 +1,6 @@
-def home_handler(query,params=None):
-    clean_params = {k:v[0] for k,v in query.items()}
-    return 200, {"status":"success", "data":clean_params}
+def home_handler(query=None,params=None):
+    return 200, {
+        "status": "success",
+        "mesage": "A RESTful CRUD API for managing a library system — built entirely in **pure Python**, with no external backend frameworks like Django or FastAPI. Uses Python's built-in `http.server` module and connects to a **PostgreSQL** database hosted on NeonDB.",
+        "live API on 🚀": "🚀 https://framework-less-library-api-utsab.onrender.com"
+        }
