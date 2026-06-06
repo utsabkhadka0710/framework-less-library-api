6191a6eef275f3174b559c8a77936fe5f042336a
Utsab Khadka <utsabkhadka9475@gmail.com>
2026-06-06 09:04:35 +0545

    fix: read_body handled malformed JSON and logs the error instead failng silently.

--- app/utils/read_body.py
+++ app/utils/read_body.py
@@ -1,9 +1,20 @@
 import json
+from json import JSONDecodeError
+from .logger import create_logger
 
+logger = create_logger(__name__)
 def read_body(handler):
     content_length = int(handler.headers.get("Content-Length",0))
+
+    if content_length == 0:
+        return {}
+
     body = handler.rfile.read(content_length)
     try:
         return json.loads(body)
-    except:
+    except JSONDecodeError as e:
+        logger.error(f"[MALFORMED JSON] Failed to parse requst boody: {e}")
         return {}
+    except Exception as e:
+        logger.error(f"[SERVER ERROR] Failed to parse requst boody: {e}")
+
--- app/utils/response.py
+++ app/utils/response.py
@@ -1,12 +1,12 @@
 import json
 
 
-def response_sender(handler, status_code=404, response_data=None):
+def response_sender(self, status_code=404, response_data=None):
 
     if not response_data:
         response_data = {"error": "Not found"}
 
-    handler.send_response(status_code)
-    handler.send_header("Content-Type","application/json")
-    handler.end_headers()
-    handler.wfile.write(json.dumps(response_data, indent=4).encode())
+    self.send_response(status_code)
+    self.send_header("Content-Type","application/json")
+    self.end_headers()
+    self.wfile.write(json.dumps(response_data, indent=4).encode())
